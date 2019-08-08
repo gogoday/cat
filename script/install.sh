78cdac0e00f946aa8e656afd806921dfe78e0734
sampsonwang <sampsonwang@tencent.com>
2019-08-08 19:40:03 +0800

    cat

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo 'start install ...'
+branch='dev'
 projectPath='/data/cat/'
 #projectPath='/Users/sampsonwang/data/cat/'
 git='https://github.com/gogoday/cat.git'
@@ -8,7 +9,7 @@ mkdir -p ${projectPath}
 cd ${projectPath} 
 # clone
 git clone ${git} ./
-git checkout -b release origin/release
+git checkout -b release origin/${branch}
 npm install -g cnpm --registry=https://registry.npm.taobao.org
 cnpm i 
 cnpm i pm2 -g
