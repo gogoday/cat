c3ff4f7e067fe3de1721103a9830f6119ddd7d9a
gogoday <wsf_1234567@126.com>
2019-08-08 19:42:34 +0800

    update

--- script/install.sh
+++ script/install.sh
@@ -9,7 +9,7 @@ mkdir -p ${projectPath}
 cd ${projectPath} 
 # clone
 git clone ${git} ./
-git checkout -b release origin/${branch}
+git checkout -b dev origin/${branch}
 npm install -g cnpm --registry=https://registry.npm.taobao.org
 cnpm i 
 cnpm i pm2 -g
