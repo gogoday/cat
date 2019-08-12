1300925fdff4221f495cb968de6f25461f83b007
gogoday <404645486@qq.com>
2019-08-12 22:48:09 +0800

    add

--- src/server/admin/service.ts
+++ src/server/admin/service.ts
@@ -6,8 +6,11 @@ class Service {
     return async (ctx, next) => {
       console.log('start git pull ... ')
       const env = ctx.query.env || 'dev';
-      const result = await child_process.execFileSync(config.gitPullSh + ` ${env}` )
-      ctx.body = result
+      // const result = await child_process.execFileSync(config.gitPullSh + ` ${env}` )
+      const result = await child_process.execFileSync(config.gitPullSh)
+      console.log(result)
+      // ctx.header
+      ctx.body = result.toString('utf-8')
     }
   }
 }
